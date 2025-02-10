bfe0f20750a86784cd9e6672f3119cf7512e2ced
aleixrenom <aleixrenom@gmail.com>
2025-02-10 16:14:20 +0200

    Changing title and favicon + set up for random header feature

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -17,19 +17,55 @@ const lists: ListProps[] = [
   },
 ];
 
+const Name = () => <span className="font-semibold">Aleix</span>;
+
+const slogans = [
+  <p>
+    Hi, I'm <Name />
+  </p>,
+  <p>
+    <Name />, the real critical hit
+  </p>,
+  <p>
+    Some call me <Name />
+  </p>,
+  <p>
+    When life gives you lemons, get yourself an <Name />
+  </p>,
+  <p>
+    The name's Renom, <Name /> Renom
+  </p>,
+  <p>
+    Welcome to <Name />
+    's emporium!
+  </p>,
+  <p>
+    Who's this <Name /> everyone's talking about?
+  </p>,
+  <p>
+    Your next hire will be called <Name />
+  </p>,
+  <p>
+    Beleive in the <Name />
+  </p>,
+];
+
 const AboutMe = () => {
+  const [num, setNum] = React.useState(0);
   return (
     <div className="flex flex-col">
       <ProfilePicAndIntro
         imageAddress={data.photoLink}
         imageAlt="Profile picture"
         text={data.introduction}
-        textHeader={
-          <p>
-            Hi, my name is <span className="font-semibold">Aleix</span>
-          </p>
-        }
+        textHeader={slogans[num]}
       />
+      {/* <button
+        onClick={() => setNum(num + 1)}
+        className="bg-secondary text-primary w-1/12"
+      >
+        Next header
+      </button> */}
       <ListGroup lists={lists} />
     </div>
   );
